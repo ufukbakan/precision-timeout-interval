9f8ebd56833a1a5554e26258a04df0faecaf3212
ufuk.bakan-p <ufuk.bakan-p@tubitak.gov.tr>
2022-07-31 02:24:32 +0300

    3.1.1. Type Hotfix
    Typescript interval controller type hotfix

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ declare function prcTimeout(milliseconds:number, callback: Function) : void;
 declare function prcTimeoutWithDelta(milliseconds:number, callback: Function) : void;
 type IntervalController = {
     end: boolean,
+    interval: number,
     callback: Function
 }
 declare function prcInterval(milliseconds:number, callback:Function) : IntervalController;
